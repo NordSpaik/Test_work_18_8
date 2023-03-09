f6de3ff2bec884a876c76f74ffb57edc009fa39b
Yuriy <113469304+NordSpaik@users.noreply.github.com>
2023-03-09 22:46:10 +0300

    Update main.py
    Почти готово

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ while count < summ_tickets:
     count += 1
     age = int(input("Введите возраст участника - "))
     print(age)
+    if 18 <= age < 25:
+        summ_money += 990
+        print(summ_money)
+    elif age >=25:
+        summ_money += 1390
+        print(summ_money)
+    else:
+        print(summ_money)
+
+print("Итоговая стоимость билетов равна:")
+print(summ_money)
