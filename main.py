07e1a17e117815ffdf34be27c965a2b5982fecca
Yuriy <113469304+NordSpaik@users.noreply.github.com>
2023-03-09 22:38:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Программа, которая будет подсчитывать общую стоимость билетов на мероприятие
 
-print("Введите желаемое количество блетов на мероприятие")
-summ_tickets = input()
+summ_tickets = int(input("Введите желаемое количество билетов на мероприятие - "))
 print(summ_tickets)
+
+count = 0
+summ_money = 0
+
+while count < summ_tickets:
+    count += 1
+    age = int(input("Введите возраст участника - "))
+    print(age)
